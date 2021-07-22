d55b32ca3f51b6aac73ac78754b1b66dc6522391
Avik S <eml2avik@gmail.com>
2021-07-22 13:24:42 +0530

    added Flag checks

--- flags.go
+++ flags.go
@@ -56,6 +56,7 @@ func validPointerToStruct(flags interface{}) (r reflect.Type, err error) {
 }
 
 func BuildFlagsStruct(flags interface{}) (err error) {
+	// given a struct pointer this function populates its elements with enumerated flags
 	structTypeObj, err := validPointerToStruct(flags)
 	if err != nil {
 		return err
@@ -83,25 +84,63 @@ func BuildFlagsStruct(flags interface{}) (err error) {
 	}
 	structValueObj := reflect.ValueOf(flags).Elem()
 	for i := 0; i < minimum; i++ {
-		fieldName := structTypeObj.Field(i).Name
-		FieldValue := structValueObj.FieldByName(fieldName)
-		if !FieldValue.CanSet() {
+		// fieldName := structTypeObj.Field(i).Name
+		// FieldValue := structValueObj.FieldByName(fieldName)
+		fieldValue := structValueObj.Field(i)
+		if !fieldValue.CanSet() {
 			return fmt.Errorf("Invalid Struct Object")
 		}
-		if FieldValue.Type() != ptype {
+		if fieldValue.Type() != ptype {
 			return fmt.Errorf("Inconsistent types of str")
 		}
-		FieldValue.Set(reflect.ValueOf(constArray[i]).Convert(ptype))
+		fieldValue.Set(reflect.ValueOf(constArray[i]).Convert(ptype))
 	}
 	return err
 }
 
-func GetFlagComponents(flagStruct interface{}, flagSum interface{}) []interface{} {
-	// tbd
-	return []interface{}{}
+func GetFlagComponents(testSum interface{}) (I []interface{}) {
+	// Given a sum value of any integer types this function returns an array
+	// of its components
+	origType := reflect.TypeOf(testSum)
+	// kind := origType.Kind()
+	int_type := reflect.TypeOf(constArray[0])
+	fmt.Println()
+	if !reflect.ValueOf(testSum).Type().ConvertibleTo(int_type) {
+		return I
+	}
+	testSumRef := reflect.ValueOf(testSum).Convert(int_type)
+	cumulative := 0
+	for i := 0; i < 64; i++ {
+		testSumVal := testSumRef.Interface().(int)
+		current := constArray[i]
+		hasFlag := testSumVal&current == current
+		if hasFlag == true {
+			I = append(I, reflect.ValueOf(current).Convert(origType).Interface())
+			cumulative |= current
+			if cumulative >= testSumVal {
+				break
+			}
+		}
+	}
+	return I
 }
 
-func ValidateComponentsInSum(flagStruct interface{}, flagSum interface{}) (err error) {
-	//tbd
-	return err
+func FlagInSum(flag interface{}, flagSum interface{}) (bool, interface{}) {
+	// this function checks wheter a flag is present in a sum of flags
+	// return  value contains boolean result for presence of flag in flagSum and
+	// Leftover of FlagSum if True else nil
+	int_type := reflect.TypeOf(0)
+	fv := reflect.ValueOf(flag)
+	fsv := reflect.ValueOf(flagSum)
+	if !fv.Type().ConvertibleTo(int_type) || !fsv.Type().ConvertibleTo(int_type) {
+		panic("the type of inputs must be any integer types")
+	}
+	fvI := fv.Convert(int_type).Interface().(int)
+	fsvI := fsv.Convert(int_type).Interface().(int)
+	truth := (fvI & fsvI) == fvI
+	if truth == true {
+		leftover := reflect.ValueOf(fvI ^ fsvI)
+		return true, leftover.Convert(fsv.Type()).Interface()
+	}
+	return false, nil
 }
--- flags_test.go
+++ flags_test.go
@@ -4,15 +4,41 @@ import (
 	"testing"
 )
 
+var s = struct {
+	FLa, FLb, FLc, FLd, FLe, FLf, FLg int8 // works with any other integer types
+}{}
+
 func TestBuildFlags(t *testing.T) {
-	var s = struct {
-		FLa, FLb, FLc, FLd, FLe, FLf, FLg int8 // works with any other integer types
-	}{}
-	err := BuildFlagsStruct(&s)
+	err := BuildFlagsStruct(&s) // need pointer
 	if err != nil {
 		t.Fatalf("%s", err)
 	}
 	if (s.FLa | s.FLb | s.FLc | s.FLd | s.FLe | s.FLf | s.FLg) != 127 {
 		t.Fatal("inconsistent flag enumeration")
+	}
+}
+
+func TestGetFlagComponents(t *testing.T) {
+	BuildFlagsStruct(&s)
+	data := GetFlagComponents(s.FLb | s.FLd | s.FLg) // need to be in the ascending order for this test case
+	if len(data) != 3 {
+		t.Fatal("Inconsistent number of components returned")
+	}
+	params := []int8{s.FLb, s.FLd, s.FLg}
+	for i := range data {
+		if params[i] != data[i].(int8) { // the same datatype as the input
+			t.Fatalf("Flag %v is not present in the component slice", params[i])
+		}
+	}
+}
+
+func TestFlagInSum(t *testing.T) {
+	truth, left := FlagInSum(int8(4), uint8(5))
+	if truth != true || left.(uint8) != 1 {
+		t.Fatal("1. Unexpected result in FlagInSum")
+	}
+	truth, left = FlagInSum(s.FLd|s.FLa, s.FLa|s.FLc|s.FLg)
+	if truth != false || left != nil {
+		t.Fatal("2. Unexpected result in FlagInSum")
 	}
 }
